a7d627a848b48d093487ba8a7960169d035385b6
Kim Burgess <kim@acaprojects.com>
2017-05-02 15:39:36 +1000

    ensure single bootstrap script instance and remove ambiguous types

--- src/app/services/skype-sdk.service.ts
+++ src/app/services/skype-sdk.service.ts
@@ -1,33 +1,60 @@
 import { Injectable } from '@angular/core';
-
 import { inject } from '../util/script-loader';
-
 import config from './skype-sdk.config';
 
+
+interface SkypeBootstrapper {
+    initialize: (config: {apiKey: string},
+                 loaded: (apiInstance) => void,
+                 error?: (reason?: any) => void) => void;
+}
+
+interface SkypeClient {}
 
 @Injectable()
 export class SkypeSDKService {
 
-    app: any;
+    client: SkypeClient;
 
     constructor() {
         this.initialise(config.apiKey).then(console.log);
     }
 
-    initialise(apiKey: string): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
+    initialise(apiKey: string): Promise<SkypeClient> {
+        // Get the SkypeWebSDK bootstrapper
+        const bootstrap = () => new Promise<SkypeBootstrapper>((resolve, reject) => {
+            const sdk = () => (<any>window).Skype as SkypeBootstrapper;
 
-            // FIXME: check for existint of global Skype object first and only
-            // load the bootstrap script if required
-            inject(config.bootstrapURL).then(() => {
-                const sdk = (<any>window).Skype;
+            if (sdk()) {
+                resolve(sdk());
+            }
 
-                sdk.initialize({apiKey: config.apiKey}, api => {
-                    this.app = api.application();
-                    resolve(this.app);
-                }, reject);
-
-            }).catch(reject);
+            inject(config.bootstrapURL)
+                .then(() => resolve(sdk()))
+                .catch(() => reject('could not load Skype SDK from CDN'));
         });
+
+        // Initialise a Skype SDK application instance
+        const initClient = (sdk: SkypeBootstrapper) =>
+            new Promise<SkypeClient>((resolve, reject) =>
+                sdk.initialize({apiKey: config.apiKey}, api => {
+                    const client = new api.application();
+                    resolve(client);
+                }, reject)
+            );
+
+        return new Promise<SkypeClient>((resolve, reject) =>
+            bootstrap()
+                .then(initClient)
+                .then((client) => {
+                    this.client = client;
+                    console.log('Skype client initialised');
+                    resolve(client);
+                })
+                .catch((err) => {
+                    console.error(`Error initialising Skype client: ${err}`);
+                    reject(err);
+                })
+        );
     }
 }
